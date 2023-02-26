757f785be9c183481cd5d3cbafe3a2c0c9fbf3ec
Luke D. Jones <luke@ljones.dev>
2023-02-27 10:44:07 +1300

    Blargh

--- package/python-octoprint/python-octoprint.mk
+++ package/python-octoprint/python-octoprint.mk
@@ -4,16 +4,11 @@
 #
 ################################################################################
 
-PYTHON_OCTOPRINT_VERSION = 1.8.6
+PYTHON_OCTOPRINT_VERSION = 1.8.0
 PYTHON_OCTOPRINT_SOURCE = OctoPrint-$(PYTHON_OCTOPRINT_VERSION).tar.gz
-PYTHON_OCTOPRINT_SITE = https://files.pythonhosted.org/packages/fc/91/f0c0afa62c7b7ad8a0fb5345b177643086cff1e4d766c9fe7d2cc103f9d6
+PYTHON_OCTOPRINT_SITE = https://files.pythonhosted.org/packages/e5/4d/65b4fb681646baca9875e09cae7bad80265dddca880503d069cd43c82212
 PYTHON_OCTOPRINT_SETUP_TYPE = setuptools
 PYTHON_OCTOPRINT_LICENSE = GNU Affero General Public License v3
 PYTHON_OCTOPRINT_LICENSE_FILES = LICENSE.txt
-
-define PYTHON_OCTOPRINT_USERS
-	foo -1 bar -1 !=blabla /home/foo /bin/sh alpha,bravo Foo user
-	octoprint -1 octoprint -1 =octoprint /home/octoprint /bin/sh - -
-endef
 
 $(eval $(python-package))
--- package/python-zipstream-ng/python-zipstream-ng.mk
+++ package/python-zipstream-ng/python-zipstream-ng.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_ZIPSTREAM_NG_VERSION = 1.4.0
+PYTHON_ZIPSTREAM_NG_VERSION = 1.3.4
 PYTHON_ZIPSTREAM_NG_SOURCE = zipstream-ng-$(PYTHON_ZIPSTREAM_NG_VERSION).tar.gz
-PYTHON_ZIPSTREAM_NG_SITE = https://files.pythonhosted.org/packages/2b/7b/d01e209c808e5eed0310f19adb647c154f4702b70e7d1b1132277048decc
+PYTHON_ZIPSTREAM_NG_SITE = https://files.pythonhosted.org/packages/8f/ea/9436abb4d2aa1e2f91ead252377e33aea6d95506c47c8feec0018e105d4d
 PYTHON_ZIPSTREAM_NG_SETUP_TYPE = setuptools
 PYTHON_ZIPSTREAM_NG_LICENSE = GNU Lesser General Public License v3 (LGPLv3)
 PYTHON_ZIPSTREAM_NG_LICENSE_FILES = LICENSE
